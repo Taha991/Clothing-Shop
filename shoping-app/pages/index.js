225de8917e403d610814a21e0847997d636188dc
Taha991 <tahamohamedman@gmail.com>
2022-10-06 08:54:42 +0200

    Adding Banner

--- shoping-app/pages/index.js
+++ shoping-app/pages/index.js
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {Button}from "../components/elements/Button"
+import { Banner } from '../components/Banner'
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Button variant="dark">This is Btn!</Button>
+      <Banner/>
     </div>
   )
 }
