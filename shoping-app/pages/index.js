58f2cad07e22e5385afe6ceeb656b49d77be920e
Taha991 <tahamohamedman@gmail.com>
2022-10-06 09:20:23 +0200

    Adding Categories

--- shoping-app/pages/index.js
+++ shoping-app/pages/index.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {Button}from "../components/elements/Button"
 import { Banner } from '../components/Banner'
+import { CategoriesSection } from '../components/CategoriesSection'
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Banner/>
+      <CategoriesSection />
     </div>
   )
 }
