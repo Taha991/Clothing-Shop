ffb913810af03cf7c3bf5b9d482adb084d54e0b9
Taha991 <tahamohamedman@gmail.com>
2022-10-07 07:09:08 +0200

    Adding Search Page && Data for images

---
+++ shoping-app/pages/search.js
@@ -0,0 +1,12 @@
+import { SearchBar } from "../components/search/SearchBar"
+
+const SearchPage = () => {
+  return(
+    <div>
+      <h1 className="text-lg p-4 text-center font-graphikLight">Search</h1>
+      <SearchBar/>
+    </div>
+  )
+}
+
+export default SearchPage
