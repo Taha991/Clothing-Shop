87904f3c409cbac3f209a3b72529055b97f4715a
Taha991 <tahamohamedman@gmail.com>
2022-10-08 06:37:56 +0200

    adding filterBar + Creating Pagination

---
+++ shoping-app/components/elements/CheckBox.jsx
@@ -0,0 +1,9 @@
+export const CheckBox = ({ checked , label , onChange }) => {
+  return(
+    <div className="flex items-center">
+      <input checked={checked} id="checked-checkbox" onChange={onChange} type="checkbox" 
+      className="w-4 h04 text-indigo-600 bg-gray-100 rounded vorder-gray-300 focus:ring-blue-500 focus:ring-2" />
+      <label className="ml-2 text-sm font-medium text-gray-900 ">{label}</label> 
+    </div>
+  )
+}
