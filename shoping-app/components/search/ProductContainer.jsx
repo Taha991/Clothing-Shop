8e10cc873be7fe0344c8292925492b214d4f1827
Taha991 <tahamohamedman@gmail.com>
2022-10-08 09:43:52 +0200

    updating next

--- shoping-app/components/search/ProductContainer.jsx
+++ shoping-app/components/search/ProductContainer.jsx
@@ -1,16 +1,18 @@
-import Image from "next/image"
+import Image from "next/image";
 
+const imageLoader = ({ src }) => {
+    return `/images/products/${src}`
+}
 
-const imageLoader = ({src}) => {
-  return `/images/products/${src}`
+const ProductContainer = ({ name, imageUrl }) => {
+    return (
+        <div className="w-full text-center rounded">
+            <div className="border rounded border-slate-300">
+                <Image loader={imageLoader} src={`${imageUrl}`} alt="product image" width={150} height={300} objectFit="cover" />
+            </div>
+            <p>{name}</p>
+        </div>
+    )
 }
-export const ProductContainer = ({name , imageUrl }) => {
-  return(
-    <div className="w-full text-center rounded">
-      <div className="border border-slate-300 rounded">
-        <Image loader={imageLoader} src={imageUrl} alt="product image" width="150" height={300} objectFit="cover" /> 
-      </div>
-      <p>{name}</p>
-    </div>
-  )
-}
+
+export default ProductContainer;
